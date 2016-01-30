1524bf75d60def2d4c5f2b7350796213d215bc93
Anton Shchukin <a.a.shchukin@gmail.com>
2016-01-30 10:28:43 +0700

    Init variables

--- js/form.js
+++ js/form.js
@@ -14,4 +14,19 @@
     evt.preventDefault();
     formContainer.classList.add('invisible');
   };
+
+})();
+
+
+
+(function() {
+
+  var LOWEST_POSITIVE_GRADE = 3;
+
+  var formElement = document.querySelector('.review-form');
+  var nameElement = formElement.querySelector('#review-name');
+  var textElement = formElement.querySelector('#review-text');
+  var markElement = formElement.querySelectorAll('[name="review-mark"]');
+
+
 })();
