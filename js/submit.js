ce3baba6fcc8ef58ef8466061d35425c8ae2f95a
Anton Shchukin <a.a.shchukin@gmail.com>
2016-01-31 01:17:30 +0700

    Get values for cookies

--- js/submit.js
+++ js/submit.js
@@ -2,4 +2,24 @@
 
 (function() {
   
+  var formElement = document.querySelector('.review-form');
+
+  formElement.onsubmit = function() {
+    event.preventDefault();
+
+    var BIRTH_MONTH = 10;
+    var BIRTH_DAY = 27;
+
+    var currentYear = new Date().getFullYear();
+    var birthdayThisYear = new Date(currentYear,  BIRTH_MONTH - 1, BIRTH_DAY).getTime();
+    var currentDate = Date.now();
+
+    var cookiesExpire = currentDate > birthdayThisYear ? currentDate - birthdayThisYear : currentDate - birthdayThisYear + (1000 * 60 * 60 * 24 * 365);
+
+    var markCookie = formElement.querySelector('[name="review-mark"]:checked').value;
+    var nameCookie = formElement.querySelector('#review-name').value;
+
+
+    formElement.submit();
+  }
 })();
