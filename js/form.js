68ab061513496894bed6d1771d3d2fa0dafab6b6
Anton Shchukin <a.a.shchukin@gmail.com>
2016-01-30 10:45:12 +0700

    Show popup

--- js/form.js
+++ js/form.js
@@ -5,10 +5,10 @@
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
 
-  formOpenButton.onclick = function(evt) {
-    evt.preventDefault();
+  //formOpenButton.onclick = function(evt) {
+  //  evt.preventDefault();
     formContainer.classList.remove('invisible');
-  };
+  //};
 
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
