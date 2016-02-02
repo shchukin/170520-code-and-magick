27abaf1c2d1f621541c532309d4d989ebaf0f1ae
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-02 11:56:13 +0700

    Reorganize variables

--- js/submit.js
+++ js/submit.js
@@ -2,7 +2,8 @@
 
 (function() {
 
-  /* Get cookies */
+  var BIRTH_MONTH = 10;
+  var BIRTH_DAY = 27;
 
   var formElement = document.querySelector('.review-form');
   var nameElement = formElement.querySelector('#review-name');
@@ -10,6 +11,9 @@
 
   var markCookie = docCookies.getItem('mark');
   var nameCookie = docCookies.getItem('name');
+
+
+  /* Get cookies */
 
   if ( markCookie ) {
     for ( var i = 0; i < markElements.length; i++ ) {
@@ -23,26 +27,20 @@
   }
 
 
-
-
-
-
   /* Set cookies */
 
   formElement.onsubmit = function() {
     event.preventDefault();
-    var BIRTH_MONTH = 10;
-    var BIRTH_DAY = 27;
 
     var currentYear = new Date().getFullYear();
     var birthdayThisYear = new Date(currentYear,  BIRTH_MONTH - 1, BIRTH_DAY).getTime();
     var currentDate = Date.now();
 
     var cookiesExpire = currentDate > birthdayThisYear ? currentDate - birthdayThisYear : currentDate - birthdayThisYear + (1000 * 60 * 60 * 24 * 365);
-    var cookiesExpireFormatted = new Date(cookiesExpire).toUTCString();
+    //var cookiesExpireFormatted = new Date(cookiesExpire).toUTCString();
 
-    var markCookie = formElement.querySelector('[name="review-mark"]:checked').value;
-    var nameCookie = formElement.querySelector('#review-name').value;
+    markCookie = formElement.querySelector('[name="review-mark"]:checked').value;
+    nameCookie = formElement.querySelector('#review-name').value;
 
     document.cookie = 'mark=' + markCookie + ';'// + ' expires=' + cookiesExpireFormatted + ';';
     document.cookie = 'name=' + nameCookie + ';'// + ' expires=' + cookiesExpireFormatted + ';';
