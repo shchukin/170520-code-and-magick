6123b86a150ebf0eeaa79e3ab1892bed4b02aa5b
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-03 12:39:40 +0700

    Set up grade

--- js/feedback.js
+++ js/feedback.js
@@ -8,8 +8,25 @@
 
     var template = document.querySelector('#review-template');
     var element = 'content' in template ? template.content.children[0].cloneNode(true) : template.children[0].cloneNode(true);
+    
+    var grade;
 
+    switch (data.rating) {
+      case 2:
+        grade = 'two';
+        break;
+      case 3:
+        grade = 'three';
+        break;
+      case 4:
+        grade = 'four';
+        break;
+      case 5:
+        grade = 'five';
+        break;
+    }
 
+    element.querySelector('.review-rating').className += data.rating >= 2 ? ' review-rating-' + grade : '';
     element.querySelector('.review-text').textContent = data.description;
 
 
