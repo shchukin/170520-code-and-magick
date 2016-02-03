d26c2e827921040b6c2e9c2ed87d44157fd8d6b3
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-03 11:52:55 +0700

    Draft of data output

--- js/feedback.js
+++ js/feedback.js
@@ -1,10 +1,31 @@
 'use strict';
 
+  var container = document.querySelector('.reviews-list');
+
+  function getElementFromTemplate(data) {
+    var template = document.querySelector('#review-template');
+    var element = template.content.children[0].cloneNode(true);
+
+    element.querySelector('.review-text').textContent = data.description;
+
+    return element;
+  }
+
+  function reviewsOutput() {
+    if ( reviews ) {
+      reviews.forEach(function(review){
+        var element = getElementFromTemplate(review);
+        container.appendChild(element);
+      });
+    }
+  }
+
+
   var reviews = null;
 
   function __reviewsLoadCallback(data) {
     reviews = data;
-    console.log(reviews);
+    reviewsOutput();
   }
 
   var scriptElement = document.createElement('script');
