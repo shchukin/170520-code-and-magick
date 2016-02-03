2154d5315e800db911d5fd7702ca845084749fa2
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-03 12:21:12 +0700

    Simplify template element fallback

--- js/feedback.js
+++ js/feedback.js
@@ -7,14 +7,7 @@
   function getElementFromTemplate(data) {
 
     var template = document.querySelector('#review-template');
-
-    if ( 'content' in template ) {
-      var element = template.content.children[0].cloneNode(true);
-    } else {
-      var element = template.children[0].cloneNode(true);
-    }
-
-
+    var element = 'content' in template ? template.content.children[0].cloneNode(true) : template.children[0].cloneNode(true);
 
 
     element.querySelector('.review-text').textContent = data.description;
