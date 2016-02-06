7f12217bc07adf4484aa98536140a5b4d581bf44
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-06 11:53:03 +0700

    Encapsulate reviews module to anonymous function

--- js/reviews.js
+++ js/reviews.js
@@ -1,93 +1,96 @@
 'use strict';
 
-var REVIEW_AUTHOR_AVATAR_SIZE = 124;
-var LOADING_TIMEOUT = 10000;
-var FiltersElement = document.querySelector('.reviews-filter');
-FiltersElement.classList.add('invisible');
+(function() {
+
+  var REVIEW_AUTHOR_AVATAR_SIZE = 124;
+  var LOADING_TIMEOUT = 10000;
+  var FiltersElement = document.querySelector('.reviews-filter');
+  FiltersElement.classList.add('invisible');
 
 
-
-function convertGradeValueToWord( grade ) {
-  if ( grade === 1 ) {
-    return 'one';
+  function convertGradeValueToWord( grade ) {
+    if ( grade === 1 ) {
+      return 'one';
+    }
+    if ( grade === 2 ) {
+      return 'two';
+    }
+    if ( grade === 3 ) {
+      return 'three';
+    }
+    if ( grade === 4 ) {
+      return 'four';
+    }
+    if ( grade === 5 ) {
+      return 'five';
+    }
   }
-  if ( grade === 2 ) {
-    return 'two';
+  
+
+  function getElementFromTemplate(data) {
+
+    var template = document.querySelector('#review-template');
+    var reviewElement = 'content' in template ? template.content.children[0].cloneNode(true) : template.childNodes[0].cloneNode(true);
+
+    var avatarElement = reviewElement.querySelector('.review-author');
+    var ratingElement = reviewElement.querySelector('.review-rating');
+    var descriptionElement = reviewElement.querySelector('.review-text');
+
+    var avatarValue;
+    var ratingValue;
+    var descriptionValue;
+
+    var avatarLoadTimeout;
+
+
+    avatarValue = new Image();
+    avatarValue.src = data.author.picture;
+
+    avatarValue.onload = function() {
+      clearTimeout(avatarLoadTimeout);
+      avatarValue.width = REVIEW_AUTHOR_AVATAR_SIZE;
+      avatarValue.height = REVIEW_AUTHOR_AVATAR_SIZE;
+      avatarValue.alt = data.author.name;
+      avatarValue.title = data.author.name;
+      avatarValue.className = avatarElement.className;
+      reviewElement.replaceChild(avatarValue, avatarElement);
+    };
+
+    avatarValue.onerror = function() {
+      reviewElement.className += ' review-load-failure';
+    };
+
+    avatarLoadTimeout = setTimeout(function() {
+      avatarValue.src = '';
+      reviewElement.className += ' review-load-failure';
+    }, LOADING_TIMEOUT);
+
+
+    ratingValue = convertGradeValueToWord(data.rating);
+    ratingElement.className += data.rating >= 2 ? ' review-rating-' + ratingValue : '';
+
+    descriptionValue = data.description;
+    descriptionElement.textContent = descriptionValue;
+
+    return reviewElement;
   }
-  if ( grade === 3 ) {
-    return 'three';
+
+
+  function reviewsOutput() {
+
+    var containerElement = document.querySelector('.reviews-list');
+
+    reviews.forEach(function(review) {
+      var reviewValue = getElementFromTemplate(review);
+      containerElement.appendChild(reviewValue);
+    });
+
+    FiltersElement.classList.remove('invisible');
+
   }
-  if ( grade === 4 ) {
-    return 'four';
+
+  if (reviews) {
+    reviewsOutput();
   }
-  if ( grade === 5 ) {
-    return 'five';
-  }
-}
 
-function getElementFromTemplate(data) {
-
-  var template = document.querySelector('#review-template');
-  var reviewElement = 'content' in template ? template.content.children[0].cloneNode(true) : template.childNodes[0].cloneNode(true);
-
-  var avatarElement = reviewElement.querySelector('.review-author');
-  var ratingElement = reviewElement.querySelector('.review-rating');
-  var descriptionElement = reviewElement.querySelector('.review-text');
-
-  var avatarValue;
-  var ratingValue;
-  var descriptionValue;
-
-  var avatarLoadTimeout;
-
-
-  avatarValue = new Image();
-  avatarValue.src = data.author.picture;
-
-  avatarValue.onload = function() {
-    clearTimeout(avatarLoadTimeout);
-    avatarValue.width = REVIEW_AUTHOR_AVATAR_SIZE;
-    avatarValue.height = REVIEW_AUTHOR_AVATAR_SIZE;
-    avatarValue.alt = data.author.name;
-    avatarValue.title = data.author.name;
-    avatarValue.className = avatarElement.className;
-    reviewElement.replaceChild(avatarValue, avatarElement);
-  };
-
-  avatarValue.onerror = function() {
-    reviewElement.className += ' review-load-failure';
-  };
-
-  avatarLoadTimeout = setTimeout(function() {
-    avatarValue.src = '';
-    reviewElement.className += ' review-load-failure';
-  }, LOADING_TIMEOUT);
-
-
-  ratingValue = convertGradeValueToWord(data.rating);
-  ratingElement.className += data.rating >= 2 ? ' review-rating-' + ratingValue : '';
-
-  descriptionValue = data.description;
-  descriptionElement.textContent = descriptionValue;
-
-  return reviewElement;
-}
-
-
-function reviewsOutput() {
-
-  var containerElement = document.querySelector('.reviews-list');
-
-  reviews.forEach(function(review) {
-    var reviewValue = getElementFromTemplate(review);
-    containerElement.appendChild(reviewValue);
-  });
-
-  FiltersElement.classList.remove('invisible');
-
-}
-
-if (reviews) {
-  reviewsOutput();
-}
-
+})();
