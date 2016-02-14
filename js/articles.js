365de3e0ed864b07c943bc90dd3cf0d61b42370a
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-15 00:00:33 +0700

    Simplify filter function

--- js/articles.js
+++ js/articles.js
@@ -43,35 +43,28 @@
       var reviewRelevanceExpireDate = new Date();
       reviewRelevanceExpireDate.setDate(reviewRelevanceExpireDate.getDate() - REVIEW_RELEVANCE_TIME_IN_DAYS);
 
-      filteredData = data.filter(function(element) {
+      return data.filter(function(element) {
         reviewDate = new Date(element.date);
         return reviewDate > reviewRelevanceExpireDate;
-      });
-
-      filteredData.sort(function(a, b) {
+      }).sort(function(a, b) {
         dateB = new Date(b.date);
         dateA = new Date(a.date);
         return dateB - dateA;
       });
-      return filteredData;
     },
     'reviews-good': function(data) {
-      filteredData = data.filter(function(element) {
+      return data.filter(function(element) {
         return element.rating >= LOWEST_POSITIVE_GRADE;
-      });
-      filteredData.sort(function(a, b) {
+      }).sort(function(a, b) {
         return b.rating - a.rating;
       });
-      return filteredData;
     },
     'reviews-bad': function(data) {
-      filteredData = data.filter(function(element) {
+      return data.filter(function(element) {
         return element.rating < LOWEST_POSITIVE_GRADE;
-      });
-      filteredData.sort(function(a, b) {
+      }).sort(function(a, b) {
         return a.rating - b.rating;
       });
-      return filteredData;
     },
     'reviews-popular': function(data) {
       filteredData = data.slice(0);
