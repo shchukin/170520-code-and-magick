ddad97cb0812fbf66bc8587c5333b80c21510fdf
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-15 15:50:20 +0700

    Encapsulate game code with anonymouse function

--- js/game.js
+++ js/game.js
@@ -1,120 +1,121 @@
 'use strict';
 
-var NOTIFICATION_FONT = 16;
-var NOTIFICATION_LINE = 20;
-var NOTIFICATION_FAMILY = 'PT Mono';
-var NOTIFICATION_COLOR = '#000000';
-
-var NOTIFICATION_WIDTH = 500;
-var NOTIFICATION_HEIGHT = 200;
-var NOTIFICATION_BACKGROUND = '#FFFFFF';
-
-var SHADOW_SIZE = 10;
-var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
-
-
-function drawBubble(ctx) {
-  ctx.fillStyle = SHADOW_COLOR;
-  ctx.fillRect(
-    ( (ctx.canvas.width - NOTIFICATION_WIDTH) / 2 ) + SHADOW_SIZE,
-    ( (ctx.canvas.height - NOTIFICATION_HEIGHT) / 2 ) + SHADOW_SIZE,
-    NOTIFICATION_WIDTH,
-    NOTIFICATION_HEIGHT
-  );
-
-  ctx.fillStyle = NOTIFICATION_BACKGROUND;
-  ctx.textBaseline = 'top';
-  ctx.fillRect(
-    (ctx.canvas.width - NOTIFICATION_WIDTH) / 2,
-    (ctx.canvas.height - NOTIFICATION_HEIGHT) / 2,
-    NOTIFICATION_WIDTH,
-    NOTIFICATION_HEIGHT
-  );
-}
-
-
-function drawAlignedTextInContainer(ctx, containerX, containerY, containerWidth, containerHeight, content, lineHeight) {
-
-  var lineQuantity = Math.min(content.length, Math.floor( containerHeight / lineHeight) );
-
-  var textWidth = containerWidth;
-
-  var textHeight = lineQuantity * lineHeight;
-
-  var textX = containerX;
-
-  var textY;
-
-  if ( containerHeight - textHeight > 0 ) {                     // if container height is enough to fit all lines
-    textY = containerY + (containerHeight - textHeight) / 2;
-  } else {                                                        // else just start from top
-    textY = containerY;
-  }
-
-  for ( var line = 0; line < lineQuantity; line++ ) {
-    ctx.fillText(content[line], textX + (textWidth / 2), textY + (line * lineHeight) );
-  }
-
-}
-
-
-function drawNotification(ctx, message) {
-
-  drawBubble(ctx);
-
-  ctx.font = NOTIFICATION_FONT + 'px' + ' ' + NOTIFICATION_FAMILY;
-  ctx.fillStyle = NOTIFICATION_COLOR;
-  ctx.textAlign = 'center';
-
-  if ( typeof (message) === 'number' ) {  // in case of number
-    message = [message.toString()];
-  }
-
-  if ( typeof (message) === 'string' ) {  // in case of single string
-    message = message.split('\n');
-  }
-
-  drawAlignedTextInContainer(
-    ctx,
-    (ctx.canvas.width - NOTIFICATION_WIDTH) / 2,
-    (ctx.canvas.height - NOTIFICATION_HEIGHT) / 2,
-    NOTIFICATION_WIDTH,
-    NOTIFICATION_HEIGHT,
-    message,
-    NOTIFICATION_LINE
-  );
-
-}
-
-var cloudsElement = document.querySelector('.header-clouds');
-var demoElement = document.querySelector('.demo');
-
-var cloudsHeight = cloudsElement.getBoundingClientRect().height;
-var demoHeight = demoElement.getBoundingClientRect().height;
-
-var parallaxTimeout;
-
-cloudsElement.style.backgroundPosition = cloudsElement.getBoundingClientRect().top + 'px center';
-
-window.addEventListener('scroll', function (){
-  clearTimeout(parallaxTimeout);
-  parallaxTimeout = setTimeout(function () {
-
-    if ( Math.abs( cloudsElement.getBoundingClientRect().top ) < cloudsHeight ) {
-      cloudsElement.style.backgroundPosition = cloudsElement.getBoundingClientRect().top + 'px center';
-    }
-
-    if (demoElement.getBoundingClientRect().top + demoHeight < 0) {
-      game.setGameStatus(window.Game.Verdict.PAUSE);
-    } else {
-      game.setGameStatus(window.Game.Verdict.CONTINUE);
-    }
-
-  }, 100);
-});
-
-
 (function() {
+
+  var NOTIFICATION_FONT = 16;
+  var NOTIFICATION_LINE = 20;
+  var NOTIFICATION_FAMILY = 'PT Mono';
+  var NOTIFICATION_COLOR = '#000000';
+
+  var NOTIFICATION_WIDTH = 500;
+  var NOTIFICATION_HEIGHT = 200;
+  var NOTIFICATION_BACKGROUND = '#FFFFFF';
+
+  var SHADOW_SIZE = 10;
+  var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+
+
+  function drawBubble(ctx) {
+    ctx.fillStyle = SHADOW_COLOR;
+    ctx.fillRect(
+      ( (ctx.canvas.width - NOTIFICATION_WIDTH) / 2 ) + SHADOW_SIZE,
+      ( (ctx.canvas.height - NOTIFICATION_HEIGHT) / 2 ) + SHADOW_SIZE,
+      NOTIFICATION_WIDTH,
+      NOTIFICATION_HEIGHT
+    );
+
+    ctx.fillStyle = NOTIFICATION_BACKGROUND;
+    ctx.textBaseline = 'top';
+    ctx.fillRect(
+      (ctx.canvas.width - NOTIFICATION_WIDTH) / 2,
+      (ctx.canvas.height - NOTIFICATION_HEIGHT) / 2,
+      NOTIFICATION_WIDTH,
+      NOTIFICATION_HEIGHT
+    );
+  }
+
+
+  function drawAlignedTextInContainer(ctx, containerX, containerY, containerWidth, containerHeight, content, lineHeight) {
+
+    var lineQuantity = Math.min(content.length, Math.floor( containerHeight / lineHeight) );
+
+    var textWidth = containerWidth;
+
+    var textHeight = lineQuantity * lineHeight;
+
+    var textX = containerX;
+
+    var textY;
+
+    if ( containerHeight - textHeight > 0 ) {                     // if container height is enough to fit all lines
+      textY = containerY + (containerHeight - textHeight) / 2;
+    } else {                                                        // else just start from top
+      textY = containerY;
+    }
+
+    for ( var line = 0; line < lineQuantity; line++ ) {
+      ctx.fillText(content[line], textX + (textWidth / 2), textY + (line * lineHeight) );
+    }
+
+  }
+
+
+  function drawNotification(ctx, message) {
+
+    drawBubble(ctx);
+
+    ctx.font = NOTIFICATION_FONT + 'px' + ' ' + NOTIFICATION_FAMILY;
+    ctx.fillStyle = NOTIFICATION_COLOR;
+    ctx.textAlign = 'center';
+
+    if ( typeof (message) === 'number' ) {  // in case of number
+      message = [message.toString()];
+    }
+
+    if ( typeof (message) === 'string' ) {  // in case of single string
+      message = message.split('\n');
+    }
+
+    drawAlignedTextInContainer(
+      ctx,
+      (ctx.canvas.width - NOTIFICATION_WIDTH) / 2,
+      (ctx.canvas.height - NOTIFICATION_HEIGHT) / 2,
+      NOTIFICATION_WIDTH,
+      NOTIFICATION_HEIGHT,
+      message,
+      NOTIFICATION_LINE
+    );
+
+  }
+
+  var cloudsElement = document.querySelector('.header-clouds');
+  var demoElement = document.querySelector('.demo');
+
+  var cloudsHeight = cloudsElement.getBoundingClientRect().height;
+  var demoHeight = demoElement.getBoundingClientRect().height;
+
+  var parallaxTimeout;
+
+  cloudsElement.style.backgroundPosition = cloudsElement.getBoundingClientRect().top + 'px center';
+
+  window.addEventListener('scroll', function (){
+    clearTimeout(parallaxTimeout);
+    parallaxTimeout = setTimeout(function () {
+
+      if ( Math.abs( cloudsElement.getBoundingClientRect().top ) < cloudsHeight ) {
+        cloudsElement.style.backgroundPosition = cloudsElement.getBoundingClientRect().top + 'px center';
+      }
+
+      if (demoElement.getBoundingClientRect().top + demoHeight < 0) {
+        game.setGameStatus(window.Game.Verdict.PAUSE);
+      } else {
+        game.setGameStatus(window.Game.Verdict.CONTINUE);
+      }
+
+    }, 100);
+  });
+
+
   /**
    * @const
    * @type {number}
