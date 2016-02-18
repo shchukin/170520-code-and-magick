c79b2928fb9735ddfda0d4c5caa3c5f6755d2746
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-18 13:00:25 +0700

    Move render function to review prototype

--- js/review.js
+++ js/review.js
@@ -1,19 +1,62 @@
-function Review(data) {
-  this._data = data
+var AVATAR_MAX_LOADING_TIME = 10000;
+var REVIEW_AUTHOR_AVATAR_SIZE = 124;
+
+
+function convertGradeValueToWord( grade ) {
+  var grades = [null, 'one', 'two', 'three', 'four', 'five'];
+  return grades[grade];
 }
 
-Review.prototype = {
+(function() {
 
-  _data: '',
-
-  render: function() {
-    console.log( this._data )
+  function Review(data) {
+    this._data = data;
+    this.element = '';
   }
-};
+
+  Review.prototype.render = function() {
+    var template = document.querySelector('#review-template');
+    this.element = ( 'content' in template ) ? ( template.content.children[0].cloneNode(true) ) : ( template.childNodes[0].cloneNode(true) );
+
+    var avatarElement = this.element.querySelector('.review-author');
+    var ratingElement = this.element.querySelector('.review-rating');
+    var descriptionElement = this.element.querySelector('.review-text');
+
+    var avatarValue = new Image();
+    var ratingValue;
+    var descriptionValue;
+
+    var avatarLoadTimeout;
 
 
+    avatarValue.onload = function() {
+      clearTimeout(avatarLoadTimeout);
+      avatarValue.width = REVIEW_AUTHOR_AVATAR_SIZE;
+      avatarValue.height = REVIEW_AUTHOR_AVATAR_SIZE;
+      avatarValue.alt = this._data.author.name;
+      avatarValue.title = this._data.author.name;
+      avatarValue.className = avatarElement.className;
+      this.element.replaceChild(avatarValue, avatarElement);
+    };
 
-var singleReview = new Review('Hello, Review!');
+    avatarValue.onerror = function() {
+      this.element.className += ' review-load-failure';
+    };
 
-console.log(singleReview);
-singleReview.render();
+    avatarLoadTimeout = setTimeout(function() {
+      avatarValue.src = '';
+      this.element.className += ' review-load-failure';
+    }, AVATAR_MAX_LOADING_TIME);
+
+    avatarValue.src = this._data.author.picture;
+
+    ratingValue = convertGradeValueToWord(this._data.rating);
+    ratingElement.className += this._data.rating >= 2 ? ' review-rating-' + ratingValue : '';
+
+    descriptionValue = this._data.description;
+    descriptionElement.textContent = descriptionValue;
+  };
+
+  window.Review = Review;
+
+})();
