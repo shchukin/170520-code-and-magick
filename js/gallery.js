e6506d081060f9d41e87ac39bf19fc04144ba854
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-18 14:25:58 +0700

    Remove event from close button click

--- js/gallery.js
+++ js/gallery.js
@@ -5,17 +5,22 @@
   var Gallery = function() {
     this.element = document.querySelector('.overlay-gallery');
     this._closeButton = this.element.querySelector('.overlay-gallery-close');
+
+    this._onCloseClick = this._onCloseClick.bind(this);
   };
 
   Gallery.prototype.show = function() {
     this.element.classList.remove('invisible');
-    this._closeButton.addEventListener('click', function() {
-      this.hide();
-    }.bind(this));
+    this._closeButton.addEventListener('click', this._onCloseClick);
   };
 
   Gallery.prototype.hide = function() {
     this.element.classList.add('invisible');
+    this._closeButton.removeEventListener('click', this._onCloseClick);
+  };
+
+  Gallery.prototype._onCloseClick = function() {
+    this.hide();
   };
 
   window.Gallery = Gallery;
