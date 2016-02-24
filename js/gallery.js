03263e1cb0f883da976c3981d7661b91c671ec10
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-24 13:24:49 +0700

    Make arrows work

--- js/gallery.js
+++ js/gallery.js
@@ -16,6 +16,7 @@ var keyCodes = {
     this._closeButtonElement = this._element.querySelector('.overlay-gallery-close');
     this._arrowButtonsElement = this._element.querySelectorAll('.overlay-gallery-control');
 
+    this._current = 0;
     this._photos = [];
 
     this._onCloseClick = this._onCloseClick.bind(this);
@@ -40,7 +41,8 @@ var keyCodes = {
     /* Keyboard */
     document.addEventListener('keydown', this._onDocumentKeyDown);
 
-    this.setCurrentPicture(startFrom);
+    this._current = startFrom;
+    this.setCurrentPicture();
   };
 
   Gallery.prototype.hide = function() {
@@ -67,9 +69,19 @@ var keyCodes = {
 
   Gallery.prototype._onArrowClick = function(event) {
     if ( event.target.className.indexOf('overlay-gallery-control-left') > -1 ) {
-      console.log('Left arrow has been clicked');
+      if( this._current === 0) {
+        this._current = this._photos.length - 1;
+      } else {
+        this._current--;
+      }
+      this.setCurrentPicture();
     } else if ( event.target.className.indexOf('overlay-gallery-control-right') > -1 ) {
-      console.log('Right arrow has been clicked');
+      if (this._current === this._photos.length - 1) {
+        this._current = 0;
+      } else {
+        this._current++;
+      }
+      this.setCurrentPicture();
     }
   };
 
@@ -79,10 +91,20 @@ var keyCodes = {
         this.hide();
         break;
       case keyCodes.ArrowLeft:
-        console.log('Left key has been pressed');
+        if( this._current === 0) {
+          this._current = this._photos.length - 1;
+        } else {
+          this._current--;
+        }
+        this.setCurrentPicture();
         break;
       case keyCodes.ArrowRight:
-        console.log('Right key has been pressed');
+        if (this._current === this._photos.length - 1) {
+          this._current = 0;
+        } else {
+          this._current++;
+        }
+        this.setCurrentPicture();
         break;
     }
   };
@@ -92,13 +114,13 @@ var keyCodes = {
     this._numberTotalElement.innerHTML = photos.length;
   };
 
-  Gallery.prototype.setCurrentPicture = function(index) {
+  Gallery.prototype.setCurrentPicture = function() {
     var olderElement = this._stageElement.querySelector('img');
     if (olderElement) {
       olderElement.remove();
     }
-    this._stageElement.appendChild(this._photos[index]._element);
-    this._numberCurrentElement.innerHTML = index + 1;
+    this._stageElement.appendChild(this._photos[this._current]._element);
+    this._numberCurrentElement.innerHTML = this._current + 1;
   };
 
   window.Gallery = Gallery;
