6d012b5c0c065017ddda16134db25027c7498791
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-26 09:14:12 +0700

    Simplify inherit function

--- js/inherit.js
+++ js/inherit.js
@@ -1,13 +1,8 @@
-/* exported inherit */
-
 'use strict';
 
-function inherit(ParentClass, ChildClass) {
-
+window.inherit = function(ParentClass, ChildClass) {
   var TempConstructor = function() {};
   TempConstructor.prototype = ParentClass.prototype;
-
   ChildClass.prototype = new TempConstructor();
   ChildClass.prototype.constructor = ChildClass;
-
-}
+};
