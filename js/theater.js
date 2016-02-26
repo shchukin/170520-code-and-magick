3000e6e1a649004c79a867d2fdabb4ee839013a9
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-26 10:51:44 +0700

    Rewrite theater module

--- js/theater.js
+++ js/theater.js
@@ -5,41 +5,22 @@
 
 (function() {
 
-  var Theater = function() {
-    this.elements = document.querySelectorAll('.photogallery-image');
-    this.data = [].map.call(this.elements, function(item, index) {
-      return new Photo(item.querySelector('img').currentSrc, index);
+  var theaterElements = document.querySelectorAll('.photogallery-image');
+
+  var theaterData = [].map.call(theaterElements, function(item) {
+    return new Photo(item.querySelector('img').currentSrc);
+  });
+
+  var theaterGallery = new Gallery();
+
+  theaterGallery.setPictures(theaterData);
+
+  [].forEach.call(theaterElements, function(element) {
+    element.addEventListener('click', function() {
+      theaterGallery.show( [].indexOf.call(theaterElements, this) );
     });
-
-    this.gallery = new Gallery();
-
-    this._onElementClick = this._onElementClick.bind(this);
-    this._init();
-  };
-
-  Theater.prototype._onElementClick = function(event) {
-    event.preventDefault();
-
-    /* get index */
-    var splitUrl = event.target.src.split('/');
-    var index = splitUrl[splitUrl.length - 1].replace('.png', '') - 1;
-
-    this.gallery.show(index);
-  };
-
-  Theater.prototype._init = function() {
-
-    [].forEach.call(this.elements, function(element) {
-      element.addEventListener('click', this._onElementClick);
-    }.bind(this));
-
-    this.gallery.setPictures(this.data);
-  };
-
-  window.Theater = Theater;
+  });
 
 })();
-
-var photos = new Theater();
 
 
