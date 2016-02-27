8206b631247117237e1c76261667c4e241e58b56
Anton Shchukin <a.a.shchukin@gmail.com>
2016-02-27 14:49:45 +0700

    Remove require.js and set up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,11 @@
 'use strict';
 
-var path = require('path');
-
 module.exports = {
-  devServer: {
-    contentBase: path.resolve(__dirname)
-  },
 
   entry: './js/main.js',
 
   output: {
-    filename: './out/main.js',
-    path: path.resolve(__dirname),
-    sourceMapFilename: '[file].map'
-  },
-
-  resolve: {
-    modulesDirectories: ['node_modules', './js', './lib', './vendor']
+    filename: './out/main.js'
   }
+
 };
